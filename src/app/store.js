235417fbaea2ababae8b4de2fd6cd229df10db6c
abhimran <abhimrann@gmail.com>
2022-10-21 21:56:41 +0600

    Created get videos feature

--- src/app/store.js
+++ src/app/store.js
@@ -4,7 +4,7 @@ import { apiSlice } from '../features/api/apiSlice';
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(apiSlice.middleware),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
